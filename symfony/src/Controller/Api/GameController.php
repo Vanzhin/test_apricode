ea3db4364477db7b23909e995ec08698654077cd
nikolay vanzhin <nikolay.vanzhin@yandex.ru>
2022-11-27 11:38:07 +0500

    GameController updated delete

--- symfony/src/Controller/Api/GameController.php
+++ symfony/src/Controller/Api/GameController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller\Api;
 
+use App\Entity\Game;
 use App\Repository\GameRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -20,22 +22,38 @@ class GameController extends AbstractController
             $request->query->getInt('page', 1)/*page number*/,
             10/*limit per page*/
         );
-        if($pagination->count()){
-            $json = $this->json(['games'=>$pagination, 'page'=>$request->query->getInt('page', 1)], 200, [], ['groups' => 'main']);
+        if ($pagination->count()) {
+            $json = $this->json(['games' => $pagination, 'page' => $request->query->getInt('page', 1)], 200, [], ['groups' => 'main']);
         } else {
-            $json = $this->json(['error'=>'no games presented', 'page'=>$request->query->getInt('page', 1)]);
+            $json = $this->json(['error' => 'no games presented', 'page' => $request->query->getInt('page', 1)]);
         }
         return ($json);
 
     }
+
     #[Route('/api/game/{id<\d+>}', name: 'app_api_game_show', methods: ['GET'])]
-    public function show(GameRepository $gameRepository, Request $request, int $id): JsonResponse
+    public function show(GameRepository $gameRepository, int $id): JsonResponse
     {
         $game = $gameRepository->find($id);
-        if($game){
+        if ($game) {
             $json = $this->json($game, 200, [], ['groups' => 'main']);
         } else {
-            $json = $this->json(['error'=>'no game presented'], 200);
+            $json = $this->json(['error' => 'no game presented'], 200);
+        }
+        return ($json);
+
+    }
+
+    #[Route('/api/game/{id<\d+>}', name: 'app_api_game_delete', methods: ['DELETE'])]
+    public function delete(int $id, EntityManagerInterface $em): JsonResponse
+    {
+        $game = $em->find(Game::class, $id);
+        if ($game) {
+            $json = $this->json(['deleted' => 'ok', 'game' => $game], 200, [], ['groups' => 'main']);
+            $em->remove($game);
+            $em->flush();
+        } else {
+            $json = $this->json(['error' => 'no game presented'], 200);
         }
         return ($json);
 
